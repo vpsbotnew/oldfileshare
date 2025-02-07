0e2390a532ae5f5debb183205a7c7efc05043595
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2025-02-08 00:26:46 +0630

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "✅ Join Channel",
                 url = client.invitelink)
         ]
     ]
@@ -131,7 +131,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = '💫 Try Again',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
